61c3dafc574a7ce14adb8d326a86c617199cab23
Pranali Rajendra Patil <158150549+pranalipatil21@users.noreply.github.com>
2026-07-11 01:17:28 +0530

    Add: set-zeroes-in-matrix - submission-1

---
+++ Data Structures & Algorithms/set-zeroes-in-matrix/submission-1.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        bool firstrowImp = false;
+        bool firstcolImp = false;
+
+        //check for first row impacted or not
+        for(int col = 0; col<n;col++){
+            if(matrix[0][col] == 0){
+                firstrowImp = true;
+                break;
+            }
+        }
+        //check for first col impacted or not
+        for(int row=0; row<m; row++){
+            if(matrix[row][0] == 0){
+                firstcolImp = true;
+                break;
+            }
+        }
+
+        //set markers in first row and first col first ele set 0
+        for(int i=1; i<m; i++){
+            for(int j=1;j<n;j++){
+                if(matrix[i][j] == 0){
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+
+        for(int i=1; i<m;i++){
+            for(int j=1;j<n;j++){
+                if(matrix[i][0] == 0 || matrix[0][j] == 0){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        if(firstrowImp){
+            for(int j=0;j<n;j++){
+                matrix[0][j] = 0;
+            }
+        }
+        if(firstcolImp){
+            for(int i=0; i<m; i++){
+                matrix[i][0] = 0;
+            }
+        }
+    }
+};
