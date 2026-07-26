aecd7c0b695683acde51bf99771d73592bce17aa
Pranali Rajendra Patil <158150549+pranalipatil21@users.noreply.github.com>
2026-07-26 11:39:51 +0530

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        int curr, next=1, next2=0;
+        for(int i=n-1;i>=0;i--){
+            curr=next+next2;
+            next2 = next;
+            next = curr;
+        }
+        return curr;
+    }
+};
